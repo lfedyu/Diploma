a332c6f5177abdf62b3974499a50d65d68907131
Liliya Fedyushko <Liliya_Fedyushko@epam.com>
2018-11-21 17:31:10 +0200

    google maps configuration part3: added getDeviceLocation

--- app/src/main/java/com/fedyushko/lilia/p0031_firstprogect/GoogleMapsActivity.java
+++ app/src/main/java/com/fedyushko/lilia/p0031_firstprogect/GoogleMapsActivity.java
@@ -2,6 +2,7 @@ package com.fedyushko.lilia.p0031_firstprogect;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -11,9 +12,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class GoogleMapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
@@ -21,9 +28,11 @@ public class GoogleMapsActivity extends AppCompatActivity implements OnMapReadyC
     private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
     private static final String COARSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1234;
+    private static final float DEFAULT_ZOOM = 15f;
 
     private Boolean locationPermissionsGranted = false;
     private GoogleMap gMap;
+    private FusedLocationProviderClient fusedLocationProviderClient;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -31,6 +40,33 @@ public class GoogleMapsActivity extends AppCompatActivity implements OnMapReadyC
         setContentView(R.layout.activity_google_maps);
         getLocationPermission();
     }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        //Toast.makeText(this, "Map is ready!", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "onMapReady: map is ready");
+        gMap = googleMap;
+
+        if (locationPermissionsGranted) {
+            getDeviceLocation();
+
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+            gMap.setMyLocationEnabled(true);
+            //to unable button find me(little round one)
+            //gMap.getUiSettings().setMyLocationButtonEnabled(false);
+        }
+    }
+
+    private void initMap(){
+        Log.d(TAG, "initMap: initializing map");
+        SupportMapFragment mapFragment = (SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.google_map);
+
+        mapFragment.getMapAsync(GoogleMapsActivity.this);
+    }
+
     //get permissions for Google Maps locations
     public void getLocationPermission(){
         Log.d(TAG, "getLocationPermission: getting location permissions");
@@ -50,19 +86,38 @@ public class GoogleMapsActivity extends AppCompatActivity implements OnMapReadyC
         }
     }
 
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        Toast.makeText(this,"Map is ready!",Toast.LENGTH_SHORT).show();
-        Log.d(TAG,"onMapReady: map is ready");
-        gMap = googleMap;
+    public void getDeviceLocation(){
+        Log.d(TAG, "getDeviceLocation: getting the device's current location");
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+        try{
+            if(locationPermissionsGranted){
+                Task location = fusedLocationProviderClient.getLastLocation();
+                location.addOnCompleteListener(new OnCompleteListener() {
+                    @Override
+                    public void onComplete(@NonNull Task task) {
+                        if(task.isSuccessful()){
+                            Log.d(TAG, "onComplete: found location!");
+                            Location currentLocation = (Location) task.getResult();
+                            //move camera to current location
+                            moveCamera(new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude()),DEFAULT_ZOOM );
+                        }
+                        else{
+                            Log.d(TAG, "onComplete: current location is null");
+                            Toast.makeText(GoogleMapsActivity.this, "Unable to get current location", Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                });
+            }
+        }catch (SecurityException e){
+            Log.d(TAG, "getDeviceLocation: SecurityException:"+ e.getMessage());
+        }
     }
 
-    private void initMap(){
-        Log.d(TAG, "initMap: initializing map");
-        SupportMapFragment mapFragment = (SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.google_map);
-
-        mapFragment.getMapAsync(GoogleMapsActivity.this);
-
+    private void moveCamera(LatLng latLng, float zoom){
+        Log.d(TAG, "moveCamera: moving the camera to lat: " + latLng.latitude + ", lng " + latLng.longitude);
+        gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,zoom));
     }
 
     @Override
